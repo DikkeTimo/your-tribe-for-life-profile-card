ba777ea79326f3b98b8886e9e69a9db9bebc33cf
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-09-06 16:19:41 +0200

    prismic working

---
+++ src/lib/slices/NamePf/index.svelte
@@ -0,0 +1,28 @@
+<!-- <script>
+  /** @type {import("@prismicio/client").Content.NamePfSlice} */
+  export let slice;
+</script>
+
+<section
+  data-slice-type={slice.slice_type}
+  data-slice-variation={slice.variation}
+>
+  Placeholder component for {slice.slice_type} (variation: {slice.variation})
+  Slices
+</section> -->
+
+<script>
+  /** @type {import("@prismicio/client").Content.NaamSlice} */
+
+  export let slice;
+</script>
+
+<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+  {#each slice.primary.name as item}
+    <p>{item.text}</p>
+  {/each}
+</section>
+
+<!-- <pre>
+	{JSON.stringify(slice, null, 2)}
+</pre> -->
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { SliceZone } from '@prismicio/svelte'
+  import { components } from '$lib/slices' 
+  export let data
+  console.log(data)
+
+  /** @type {import("@prismicio/client").Content.NamePfSlice} */
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<section>
+  <SliceZone slices={data.slices} {components} />
+</section>
