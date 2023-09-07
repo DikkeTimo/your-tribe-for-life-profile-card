512bad57ff9d2f60ee1d3fe73f94bb31ac852641
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-09-07 15:18:36 +0200

    first changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,21 +10,28 @@
 <SliceZone slices={data.document} {components} />
 
 
-<img src={data.slices[0].primary.timpf.url} alt="Image Alt Text" />
 
 <!-- 
 <h1>{data.slices[0].primary.name[0].text}</h1>
 <h2>{data.slices[0].primary.lastename[0].text}</h2> -->
 
-{#each data.slices as slice}
+<!-- {#each data.slices as slice}
   {#each slice.primary.name as item}
     <p>{item.text}</p>
   {/each}
-{/each}
+{/each} -->
 
 
 <div class="card">
-  <div class="personal_info"></div>
+  <div class="personal_info">
+    <div class="picture">
+      <img src= {data.slices[0].primary.timpf.url} alt="Image Alt Text" />
+    </div>
+    <div class="name">
+      <h1>{data.slices[0].primary.name[0].text} {data.slices[0].primary.lastename[0].text} </h1>
+    </div>
+    <div class="socials"></div>
+  </div>
   <div class="about_info"></div>
 </div>
 
@@ -36,12 +43,46 @@
     padding: 0;
   }
   :global(body) {
+    background-color: var(--backgroundcolor);
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 100%;
     height: 100vh;
   }
-  .card {
-    background-color: aqua;
-    width: 80vw;
-    height: 75vh;
+
+  :global(:root) {
+    --backgroundcolor: #09A9D1;
+    --maincolor: #444444;
+    --textcolor: #fff;
+
+    --borderr: 30px;
   }
+  .card {
+    background-color: var(--maincolor);
+    color: var(--textcolor);
+    border-radius: var(--borderr);
+    width: 85vw;
+    height: 80vh;
+  }
+
+  .personal_info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    width: 20vw;
+    height: 100%;
+  }
+
+
+
+
+  .picture img {
+    width: 100%;
+    border-radius: 50%;
+    border: 3px solid var(--textcolor);
+  }
+
+
 </style>
